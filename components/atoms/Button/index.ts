18ad4fa7202160176eabce0704693bb52da25d38
Ramadhan <hi.mramadhanrh@gmail.com>
2022-03-03 15:44:24 +0700

    Add Button Hover and Active variant style

--- components/atoms/Button/index.ts
+++ components/atoms/Button/index.ts
@@ -38,21 +38,33 @@ const ButtonConfig: TButtonConfig = {
   `,
 };
 
+const ButtonHover = css`
+  background-color: ${({ theme }) => theme.color.primary};
+  color: ${({ theme }) => theme.buttonTextColor.primary};
+  box-shadow: 3px 3px 0px 0px ${({ theme }) => theme.color.secondary};
+`;
+
+const ButtonActive = css`
+  transform: translate(1px, 1px);
+  box-shadow: 2px 2px 0px 0px ${({ theme }) => theme.color.secondary} !important;
+`;
+
 const Button = styled.button<ButtonProps>`
   border: none;
   font-weight: bold;
   transition-property: box-shadow, background-color, color;
-  transition: 0.2s cubic-bezier(0.55, 0.055, 0.675, 0.19);
+  transition: 0.05s cubic-bezier(0.55, 0.055, 0.675, 0.19);
   box-shadow: 0px 0px 0px 0px #f86f22;
 
   background-color: ${({ theme, variant }) => theme.buttonColor[variant]};
   color: ${({ theme, variant }) => theme.buttonTextColor[variant]};
-  box-shadow: none;
 
   &:hover {
-    background-color: ${({ theme }) => theme.color.primary};
-    color: ${({ theme }) => theme.buttonTextColor.primary};
-    box-shadow: 3px 3px 0px 0px ${({ theme }) => theme.color.secondary};
+    ${({ variant }) => variant !== 'link' && ButtonHover}
+  }
+
+  &:active {
+    ${({ variant }) => variant !== 'link' && ButtonActive}
   }
 
   ${({ size }) => ButtonConfig[size || 'sm']}
