8db752b194c4a1904e1580a4dbfe7d290760d269
Ramadhan <hi.mramadhanrh@gmail.com>
2022-03-03 15:46:18 +0700

    Add ProductImageSlider component

---
+++ components/organisms/ProductImageSlider/index.tsx
@@ -0,0 +1,83 @@
+import { FC, useMemo, useState } from 'react';
+import { Col, Row } from 'antd';
+import {
+  Container,
+  DotStep,
+  GradientOverlay,
+  Image,
+  StepWrapper,
+} from './style';
+
+const ProductImageSlider: FC<{ data: string[] }> = ({
+  data = [
+    'sample-modal-pic0.png',
+    'sample-modal-pic1.png',
+    'sample-modal-pic2.png',
+  ],
+}) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [isOnTransition, setOnTransition] = useState(false);
+  const [isNextDirection, setNextDirection] = useState(false);
+  const currentIndex = useMemo(() => activeIndex % data.length, [activeIndex]);
+
+  const imageList = useMemo(() => {
+    if (data.length > 0 && data.length < 3)
+      return new Array(4).fill('').map((_, i) => data[i % data.length]);
+    return data;
+  }, [data]);
+
+  const nextIndex = useMemo(
+    () => (activeIndex + 1) % imageList.length,
+    [activeIndex],
+  );
+  const prevIndex = useMemo(
+    () => (activeIndex === 0 ? imageList.length - 1 : activeIndex - 1),
+    [activeIndex],
+  );
+
+  const getOffset = (idx: number) => {
+    if (idx === activeIndex) return '0%';
+    if (idx === nextIndex) return '100%';
+    return '-100%';
+  };
+
+  const getTransition = (idx: number) => {
+    if (idx === activeIndex) return '';
+    if (isNextDirection && idx === prevIndex) return '';
+    if (!isNextDirection && idx === nextIndex) return '';
+    return '0s';
+  };
+
+  const handleTransitionEnd = () => {
+    setOnTransition(false);
+  };
+
+  const handleClick = (isNext: boolean) => {
+    if (isOnTransition) return;
+    setOnTransition(true);
+    setNextDirection(isNext);
+    setActiveIndex(isNext ? (activeIndex + 1) % imageList.length : prevIndex);
+  };
+
+  return (
+    <Container onClick={() => handleClick(true)}>
+      <GradientOverlay />
+      {imageList.map((item, i) => (
+        <Image
+          key={i.toString()}
+          style={{ transitionDuration: getTransition(i) }}
+          offset={getOffset(i)}
+          src={item}
+          onTransitionEnd={i === activeIndex ? handleTransitionEnd : undefined}
+        />
+      ))}
+      <StepWrapper>
+        {data.map((_, i) => (
+          <DotStep active={i === currentIndex} key={i.toString()} />
+        ))}
+      </StepWrapper>
+    </Container>
+  );
+};
+
+export default ProductImageSlider;
---
+++ components/organisms/ProductImageSlider/style.ts
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  position: relative;
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+  cursor: pointer;
+`;
+
+export const GradientOverlay = styled.div`
+  position: relative;
+  z-index: 1;
+  height: 100%;
+  width: 100%;
+  background: linear-gradient(
+    360deg,
+    rgba(117, 73, 7, 0.56) 0%,
+    rgba(117, 73, 7, 0) 100%
+  );
+  opacity: 1;
+  transform: translate(0, 50%);
+  transition: 0.5s cubic-bezier(0.215, 0.61, 0.355, 1);
+  transition-property: opacity, transform;
+
+  ${Container}:hover & {
+    transform: translate(0, 0%);
+    opacity: 1;
+  }
+`;
+
+export const Image = styled.img<{ offset?: number | string }>`
+  position: absolute;
+  top: 0;
+  left: ${({ offset = 0 }) => offset};
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  transition: 1s left cubic-bezier(0.215, 0.61, 0.355, 1);
+`;
+
+export const StepWrapper = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  position: absolute;
+  z-index: 1;
+  bottom: 18px;
+  left: 50%;
+  transform: translate(-50%, 0);
+`;
+
+export const DotStep = styled.div<{ active?: boolean }>`
+  margin: 0 5px;
+  width: 8px;
+  height: 8px;
+  border-radius: 4px;
+  background-color: ${({ theme }) => theme.color.white};
+  opacity: ${({ active }) => (active ? 1 : 0.5)};
+  transform: ${({ active }) => `scale(${active ? 1 : 0.8})`};
+  transition: 0.5s cubic-bezier(0.215, 0.61, 0.355, 1);
+  transition-property: opacity, transform;
+`;
